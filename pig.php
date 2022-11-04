3b212c49e60c652cba82cf1e7ff1652cf4c71997
richard <binghamrichard123@gmail.com>
2022-11-04 15:11:46 +0000

    added try catch

--- pig.php
+++ pig.php
@@ -4,15 +4,27 @@ require 'vendor/autoload.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
-
 // TODO: Validate $_GET['id]
 
-$pigService = new IoFarm\Services\PigService();
-$pig = $pigService->getPig($_GET['id']);
+try {
+    $pigService = new IoFarm\Services\PigService();
+    $pig = $pigService->getPig($_GET['id']);
 
-$data =[
-    "message" => "Successfully retrieved pig.",
-    "data" => $pig
-];
-
+    $data = [
+        "message" => "Successfully retrieved pig.",
+        "data" => $pig
+    ];
+} catch (InvalidArgumentException $e) {
+    http_response_code(400);
+    $data = [
+        "message" => "Bad request",
+        "data" => []
+    ];
+} catch (Exception $e) {
+    http_response_code(500);
+    $data = [
+        "message" => "Server error",
+        "data" => []
+    ];
+}
 echo json_encode($data, true);
