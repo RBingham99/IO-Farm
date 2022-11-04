1f87d1b21a28f5a47bf811426130940c8081611f
richard <binghamrichard123@gmail.com>
2022-11-04 12:22:04 +0000

    added PigColl class

--- src/Entities/Pig.php
+++ src/Entities/Pig.php
@@ -4,6 +4,93 @@ namespace IoFarm\Entities;
 
 class Pig
 {
+    private int $id;
+    private string $name;
+    private float $weight;
+    private string $colour;
+    private int $species; //Maybe should be a string as the int is a foreign key.
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param float $weight
+     */
+    public function setWeight(float $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColour(): string
+    {
+        return $this->colour;
+    }
+
+    /**
+     * @param string $colour
+     */
+    public function setColour(string $colour): void
+    {
+        $this->colour = $colour;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpecies(): int
+    {
+        return $this->species;
+    }
+
+    /**
+     * @param int $species
+     */
+    public function setSpecies(int $species): void
+    {
+        $this->species = $species;
+    }
+
+
     public function eat(string $food): string
     {
         return 'Mmm ' . $food;
---
+++ src/Entities/PigColl.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace IoFarm\Entities;
+
+// A collection of Pig objects.
+
+class PigColl
+{
+    private array $pigs;
+
+    /**
+     * @return array
+     */
+    public function getPigs(): array
+    {
+        return $this->pigs;
+    }
+
+    /**
+     * @param array $pigs
+     */
+    public function setPigs(array $pigs): void
+    {
+        $this->pigs = $pigs;
+    }
+
+
+}
